909e27c7c4fbde22d816bfd55fc132f2bd8311ca
Alex van Poppelen <avanpoppelen@gmail.com>
2019-01-03 23:28:54 +0000

    Move feh background stuff to proper dirs.

--- display/fehbg.sh
+++ display/fehbg.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-background="$HOME/.env/feh/bg.jpg"
-default="$HOME/.env/feh/default.jpg"
+background="$HOME/.env/media/bg.jpg"
+default="$HOME/.env/media/default.jpg"
 
 if [ -e "$background" ]; then
 	feh --no-fehbg --bg-scale "$background"
