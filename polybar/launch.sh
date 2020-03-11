33d3a01ddd4246fa57972bea4d88d1692b89f41d
Alex van Poppelen <avanpoppelen@gmail.com>
2020-03-11 10:22:26 +0000

    Fix up network interfaces for polybar launch script.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,9 +11,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Get network interfaces.
+# Get network interfaces. For wired, take the last interface if there are
+# multiple. Some USB to Ethernet adaptors create their own.
 wireless="$(iw dev | grep Interface | sed -n 1p | cut -d ' ' -f 2)"
-wired="$(ip link show | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}' | xargs)"
+wired="$(ip link show | awk -F: '$0 !~ "lo|vir|wl|tun|^[^0-9]"{print $2}' | tail -1 | xargs)"
+echo "launch.sh: Found wireless ($wireless) and wired ($wired) network interfaces."
 
 # Launch bar(s)
 if type "xrandr" > /dev/null; then
@@ -21,7 +23,7 @@ if type "xrandr" > /dev/null; then
 		MONITOR=$m WIRELESS_INTERFACE=$wireless WIRED_INTERFACE=$wired polybar main &
 	done
 else
-	polybar main &
+	WIRELESS_INTERFACE=$wireless WIRED_INTERFACE=$wired polybar main &
 fi
 
 echo "launch.sh: Polybar launched."
