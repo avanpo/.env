3aff4429c6e6e0904b49108ab5666fece60a2fa3
Alex van Poppelen <avanpoppelen@gmail.com>
2022-04-08 12:37:47 +0100

    Some polybar fixed for other setup.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -13,7 +13,12 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Get network interfaces. For wired, take the last interface if there are
 # multiple. Some USB to Ethernet adaptors create their own.
-wireless="$(iwctl device list | grep station | head -1 | awk '{print $1}')"
+wireless=""
+if command -v iwctl &> /dev/null; then
+	wireless="$(iwctl device list | grep station | head -1 | awk '{print $1}')"
+elif command -v nmcli &> /dev/null; then
+	wireless="$(nmcli d status | grep wifi | grep -v disconnected | head -1 | cut -d ' ' -f 1)"
+fi
 wired="$(ip link show | awk -F: '$0 !~ "lo|vir|wl|tun|docker|^[^0-9]"{print $2}' | tail -1 | xargs)"
 echo "launch.sh: Found wireless ($wireless) and wired ($wired) network interfaces."
 
@@ -24,10 +29,10 @@ backlight="$(basename "$(find /sys/class/backlight/ -mindepth 1 -print -quit)")"
 # Launch bar(s)
 if type "xrandr" > /dev/null; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR="$m" WIRELESS_INTERFACE="$wireless" WIRED_INTERFACE="$wired" BACKLIGHT="$backlight" polybar main &
+		MONITOR="$m" WIRELESS_INTERFACE="$wireless" WIRED_INTERFACE="$wired" BACKLIGHT="$backlight" polybar main -c "~/.config/polybar/config.ini" &
 	done
 else
-	WIRELESS_INTERFACE="$wireless" WIRED_INTERFACE="$wired" BACKLIGHT="$backlight" polybar main &
+	WIRELESS_INTERFACE="$wireless" WIRED_INTERFACE="$wired" BACKLIGHT="$backlight" polybar main -c "~/.config/polybar/config.ini" &
 fi
 
 echo "launch.sh: Polybar launched."
