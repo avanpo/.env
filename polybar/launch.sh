ca149c87aec458c1df2ff38d100e6c16ebaffad6
Alex van Poppelen <avanpoppelen@gmail.com>
2022-11-24 17:18:57 -0800

    Fix up polybar launch script.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,11 +5,11 @@
 # strange bugs.
 source $HOME/.config/bashrc
 
-# Terminate already running bar instances
+# Terminate already running bar instances.
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+today="$(date '+%Y-%m-%d')"
+log_file="/tmp/polybar.$today.log"
 
 # Get network interfaces. For wired, take the last interface if there are
 # multiple. Some USB to Ethernet adaptors create their own.
@@ -20,22 +20,19 @@ elif command -v nmcli &> /dev/null; then
 	wireless="$(nmcli d status | grep wifi | grep -v disconnected | head -1 | cut -d ' ' -f 1)"
 fi
 wired="$(ip link show | awk -F: '$0 !~ "lo|vir|wl|tun|docker|^[^0-9]"{print $2}' | tail -1 | xargs)"
-echo "launch.sh: Got wireless '$wireless' and wired '$wired' network interfaces."
+echo "launch.sh: Got wireless '$wireless' and wired '$wired' network interfaces." | tee -a "$log_file"
 
 # Get backlight card.
 backlight="$(basename "$(find /sys/class/backlight/ -mindepth 1 -print -quit)")"
-echo "launch.sh: Got backlight card '$backlight'."
+echo "launch.sh: Got backlight card '$backlight'." | tee -a "$log_file"
 
-today="$(date '+%Y-%m-%d')"
-log_file="/tmp/polybar.$today.log"
+# Wait until the processes have been shut down.
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar(s)
-if type "xrandr" > /dev/null; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR="$m" WIRELESS_INTERFACE="$wireless" WIRED_INTERFACE="$wired" BACKLIGHT="$backlight" polybar main -c "~/.config/polybar/config.ini" 2>> "$log_file" &
-	done
-else
-	WIRELESS_INTERFACE="$wireless" WIRED_INTERFACE="$wired" BACKLIGHT="$backlight" polybar main -c "~/.config/polybar/config.ini" 2>> "$log_file" &
-fi
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR="$m" WIRELESS_INTERFACE="$wireless" WIRED_INTERFACE="$wired" BACKLIGHT="$backlight" polybar main -c "~/.config/polybar/config.ini" 2>> "$log_file" &
+	echo "launch.sh Launched polybar on $m". | tee -a "$log_file"
+done
 
-echo "launch.sh: Polybar launched."
+echo "launch.sh: Script exiting." | tee -a "$log_file"
