69653669d9826b6ebdbf7acaee9143f37aa25341
Alex van Poppelen <avanpoppelen@gmail.com>
2022-11-24 19:40:07 -0800

    A bunch more work on monitor.py.
    What am I doin with my life

--- display/monitor.py
+++ display/monitor.py
@@ -10,6 +10,8 @@
 
 import subprocess
 import sys
+import time
+
 from dataclasses import dataclass
 
 MM_TO_INCH = 25.4
@@ -25,6 +27,8 @@ class Monitor:
     physical_mm_y: float = 0
 
     def dpi(self) -> int:
+        if not physical_mm_x or not physical_mm_y:
+            return 0
         dpi_x = self.res_x / (self.physical_mm_x / MM_TO_INCH)
         dpi_y = self.res_y / (self.physical_mm_y / MM_TO_INCH)
         return round((dpi_x + dpi_y) / 2.0)
@@ -37,18 +41,22 @@ def primary():
         sys.exit()
     primary = monitors[0]
 
-    cmd = ["xrandr"]
+    args = []
     # Turn off all other monitors.
     for m in monitors[1:]:
-        cmd.extend(["--output", m.name, "--off"])
-    cmd.extend(["--output", primary.name, "--auto"])
+        args.extend(["--output", m.name, "--off"])
+    args.extend(["--output", primary.name, "--auto"])
 
     print(f"Enabling {primary.name}, disabling all others.")
-    cp = subprocess.run(cmd, stdout=sys.stdout, stderr=sys.stderr)
-    if cp.returncode != 0:
-        print("Got {cp.returncode} status code from xrandr.")
+    if not run_xrandr(args):
         return False
-    return False
+
+    # Watch to get dpi info, since dimensions aren't present when the monitor
+    # is off.
+    primary = watch(primary)
+    run_xrandr(["--dpi", str(primary.dpi)])
+
+    return True
 
 
 def external():
@@ -60,20 +68,23 @@ def external():
     external = monitors[1]
 
     print(f"Enabling {external.name}, disabling {primary.name}.")
-    cp = subprocess.run([
-        "xrandr", "--output", primary.name, "--off", "--output", external.name,
-        "--auto"
-    ],
-                        stdout=sys.stdout,
-                        stderr=sys.stderr)
-    if cp.returncode != 0:
-        print("Got {cp.returncode} status code from xrandr.")
+    args = [
+        "--output", primary.name, "--off", "--output", external.name, "--auto"
+    ]
+    if not run_xrandr(args):
         return False
-    return False
+
+    # Watch to get dpi info, since dimensions aren't present when the monitor
+    # is off.
+    external = watch(external)
+    run_xrandr(["--dpi", str(external.dpi)])
+
+    return True
 
 
 def all_monitors():
     print("not yet implemented")
+    return False
 
 
 def reset():
@@ -81,9 +92,78 @@ def reset():
     cp = subprocess.run(["xrandr", "-s", "0"],
                         stdout=sys.stdout,
                         stderr=sys.stderr)
-    if cp.returncode == 0:
-        return True
-    return False
+    if cp.returncode != 0:
+        print("Got {cp.returncode} status code from xrandr.")
+        return False
+    return True
+
+
+def run_xrandr(args):
+    cp = subprocess.run(["xrandr"] + args,
+                        stdout=sys.stdout,
+                        stderr=sys.stderr)
+    if cp.returncode != 0:
+        print("Got {cp.returncode} status code from xrandr.")
+        return False
+    return True
+
+
+def extract_monitor(tokens):
+    name = tokens[0]
+    primary = False
+    if "primary" in tokens:
+        primary = True
+
+    result = Monitor(name, primary)
+
+    # What a terrible hack...
+    resolution = tokens[2]
+    if primary:
+        resolution = tokens[3]
+
+    # Get resolution
+    if "x" in resolution:
+        res_x, res_y = [int(d) for d in resolution.split("+")[0].split("x")]
+        result.res_x = res_x
+        result.res_y = res_y
+
+    # Get physical dimensions
+    if tokens[-1].endswith("mm") and tokens[-3].endswith("mm"):
+        result.physical_mm_x = float(tokens[-3][:-2])
+        result.physical_mm_y = float(tokens[-1][:-2])
+
+    return result
+
+
+def watch(m):
+    start = time.time()
+
+    no_dimensions = True
+    while no_dimensions:
+        cp = subprocess.run(["xrandr", "-q"], capture_output=True)
+        if cp.returncode != 0:
+            print(f"Failed to run xrandr: {completed_process.stderr}")
+        lines = cp.stdout.decode('utf-8').splitlines()
+
+        for line in lines:
+            if m.name not in line:
+                continue
+
+            m = extract_monitor(line.strip().split())
+            if m.res_x and m.res_y and m.physical_mm_x and m.physical_mm_y:
+                print(m)
+                no_dimensions = False
+                break
+        if no_dimensions:
+            # Don't wait forever.
+            if time.time() - start > 10:
+                print(f"Waited, but no monitor {name} dimensions found.")
+                return m
+            time.sleep(0.1)
+
+    elapsed = time.time() - start
+    print(f"Monitor {m.name} dimensions took {elapsed:.2f} seconds.")
+    return m
 
 
 def parse_xrandr():
@@ -99,29 +179,7 @@ def parse_xrandr():
         if len(tokens) < 2 or tokens[1] != "connected":
             continue
 
-        name = tokens[0]
-        primary = False
-        if "primary" in tokens:
-            primary = True
-        result.append(Monitor(name, primary))
-
-        # What a terrible a hack...
-        resolution = tokens[2]
-        if primary:
-            resolution = tokens[3]
-
-        # Get resolution
-        if "x" in resolution:
-            res_x, res_y = [
-                int(d) for d in resolution.split("+")[0].split("x")
-            ]
-            result[-1].res_x = res_x
-            result[-1].res_y = res_y
-
-        # Get physical dimensions
-        if tokens[-1].endswith("mm") and tokens[-3].endswith("mm"):
-            result[-1].physical_mm_x = float(tokens[-3][:-2])
-            result[-1].physical_mm_y = float(tokens[-3][:-2])
+        result.append(extract_monitor(tokens))
 
     return result
 
